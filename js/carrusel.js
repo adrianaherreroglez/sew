a81eb75d6e46ee3881cb9837a2212a2a848e2166
uo287543 <uo287543@uniovi.es>
2025-06-11 18:09:08 +0200

    noticias sobre el concejo hecho

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,29 +1,37 @@
 class Carrusel {
-    constructor() {
-        this.section = $('section').eq(0);
-        this.article = this.section.find('article');
+    constructor(selector) {
+        this.$section = $(selector).eq(0);
+        this.$article = this.$section.find('article');
+        this.$botones = this.$section.find('footer').find('button');
         this.imagenes = [
-            'multimedia/mapa.png',
-            'multimedia/mieres1.jpg',
-            'multimedia/mieres2.jpg',
-            'multimedia/mieres3.jpg',
-            'multimedia/mieres4.jpg'
+            'multimedia/imagenes/mapa.png',
+            'multimedia/imagenes/mieres1.jpg',
+            'multimedia/imagenes/mieres2.jpg',
+            'multimedia/imagenes/mieres3.jpg',
+            'multimedia/imagenes/mieres4.jpg'
         ];
         this.index = 0;
-        this.mostrarImagen();
 
-        const botones = this.section.find('footer').find('button');
-        botones.eq(0).on('click', () => this.anterior());
-        botones.eq(1).on('click', () => this.siguiente());
+        this.inicializar();
+    }
+
+    inicializar() {
+        this.mostrarImagen();
+        this.botones();
     }
 
     mostrarImagen() {
-        this.article.empty();
-        const imagen = $('<img>', {
+        this.$article.empty();
+        const $img = $('<img>', {
             src: this.imagenes[this.index],
             alt: `Imagen ${this.index + 1}`
         });
-        this.article.append(imagen);
+        this.$article.append($img);
+    }
+
+    botones() {
+        this.$botones.eq(0).on('click', () => this.anterior());
+        this.$botones.eq(1).on('click', () => this.siguiente());
     }
 
     anterior() {
@@ -37,6 +45,20 @@ class Carrusel {
     }
 }
 
-$(document).ready(function () {
-    new Carrusel();
-});
+class App {
+    constructor() {
+        this.esperarDOM(() => {
+            this.carrusel = new Carrusel('main section');
+        });
+    }
+
+    esperarDOM(callback) {
+        if (document.readyState === 'loading') {
+            document.addEventListener('DOMContentLoaded', callback);
+        } else {
+            callback();
+        }
+    }
+}
+
+new App();
