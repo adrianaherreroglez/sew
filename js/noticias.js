4839423dd26b1b179b156304c734f263bdb10988
uo287543 <uo287543@uniovi.es>
2025-06-14 10:11:36 +0200

    prueba para noticias

--- js/noticias.js
+++ js/noticias.js
@@ -1,12 +1,7 @@
-class NoticiasMieres {
-    constructor(apiKey, selector) {
-        this.apiKey = apiKey;
-        this.section = $(selector).eq(0);
-        this.article = this.section.find('article');
-        this.apiUrl = `https://newsapi.org/v2/everything?q=Mieres+Langreo&language=es&sortBy=publishedAt&apiKey=${this.apiKey}`;
-    }
-
-    iniciar() {
+class Noticias {
+    constructor() {
+        this.apiKey = '9ed1addf608a4ba5bd0de626859fa965';
+        this.apiUrl = 'https://newsapi.org/v2/everything?q=Mieres+Langreo&language=es&sortBy=publishedAt&apiKey=' + this.apiKey;
         this.cargarNoticias();
     }
 
@@ -14,54 +9,35 @@ class NoticiasMieres {
         $.ajax({
             url: this.apiUrl,
             method: 'GET',
+            dataType: 'json',
             success: this.mostrarNoticias.bind(this),
             error: this.mostrarError.bind(this)
         });
     }
 
     mostrarNoticias(data) {
-        const noticias = data.articles;
-        const maxNoticias = 6;
+        var noticias = data.articles;
+        var contenedor = $("main>section:last-of-type>article");
+        contenedor.empty();
 
-        for (let i = 0; i < Math.min(noticias.length, maxNoticias); i++) {
-            const noticia = noticias[i];
-            const seccionNoticia = $('<section>');
-            const cabecera = $(`<h3>${noticia.title}</h3>`);
-            const cuerpo = $('<article>').append(`<p>${noticia.description || ''}</p>`);
-            const pie = $('<footer>').append(`<a href="${noticia.url}" target="_blank">Leer más</a>`);
+        for (var i = 0; i < noticias.length; i++) {
+            var noticia = noticias[i];
+            var noticiaHTML = '<section>' +
+                '<h3>' + noticia.title + '</h3>' +
+                '<article><p>' + (noticia.description || '') + '</p></article>' +
+                '<footer><a href="' + noticia.url + '" target="_blank">Leer más</a></footer>' +
+                '</section>';
 
-            seccionNoticia.append(cabecera, cuerpo, pie);
-            this.article.append(seccionNoticia);
+            contenedor.append(noticiaHTML);
         }
     }
 
     mostrarError() {
-        this.article.append('<p>No se pudieron cargar las noticias.</p>');
+        var contenedor = $("main>section:last-of-type>article");
+        contenedor.empty();
+        contenedor.append('<p>No se pudieron cargar las noticias.</p>');
     }
 }
 
-
-class InitNoticias {
-    constructor() {
-        this.notificador = null;
-        this.apiKey = '9ed1addf608a4ba5bd0de626859fa965';
-        this.selector = 'main section:nth-of-type(2)';
-        this.inicializar();
-    }
-
-    inicializar() {
-        // Sin pasar funciones anónimas: se llama a método de instancia
-        if (document.readyState === 'loading') {
-            document.addEventListener('DOMContentLoaded', this.iniciar.bind(this));
-        } else {
-            this.iniciar();
-        }
-    }
-
-    iniciar() {
-        this.notificador = new NoticiasMieres(this.apiKey, this.selector);
-        this.notificador.iniciar();
-    }
-}
-
-new InitNoticias();
+// Simplemente instanciamos la clase:
+new Noticias();
